e461229fe672c3b3e4e519f5bb63f92a032a0351
Viktor Mörsell <viktor.morsell@northvolt.com>
2021-04-20 00:38:31 +0200

    Add data to index page

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,54 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Link from "next/link";
+import graphcms, { gql } from "../lib/graphcms";
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+export default ({ shows, groups }) => (
+  <>
+    <h1>Shower</h1>
+    <ul>
+      {shows.map((s) => (
+        <li>
+          <Link key={s.slug} href={`/show/${s.slug}`}>
+            <a>
+              {s.title} eller {s.orTitle}
+            </a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+    <h1>Grupper</h1>
+    <ul>
+      {groups.map((g) => (
+        <li>
+          <Link key={g.slug} href={`/group/${g.slug}`}>
+            <a>{g.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </>
+);
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+export async function getStaticProps() {
+  const { shows, groups } = await graphcms.request(
+    gql`
+      {
+        shows {
+          slug
+          title
+          orTitle
+        }
+        groups {
+          slug
+          name
+        }
+      }
+    `
+  );
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+  return {
+    props: {
+      shows,
+      groups,
+    },
+  };
 }
